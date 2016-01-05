7f1016e5b45919b9003b2ef1a0112073c3fb047f
htamayo93 <htamayo@student.rcc.edu>
2016-01-05 11:48:22 -0800

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

---
+++ Assignments/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Heidy Tamayo
+ * Created on January 5, 2016, 11:39 AM
+ * Purpose: Big C
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//*User Libraries
+ 
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    char letter='C';
+       
+    //Calculate or map inputs to outputs
+    
+    
+    //Output the results
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    
+    //Exit stage right
+    return 0;
+}
