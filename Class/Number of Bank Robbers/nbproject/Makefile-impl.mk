83d796af744e53c7a985fc1076ea19eeca3c0377
htamayo93 <htamayo@student.rcc.edu>
2016-01-06 11:59:20 -0800

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

--- Class/Number of Bank Robbers/nbproject/Makefile-impl.mk
+++ Class/Number of Bank Robbers/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Gaddis_8thEd_Chap2_Prob18
+PROJECTNAME=Number of Bank Robbers
 
 # Active Configuration
 DEFAULTCONF=Debug
