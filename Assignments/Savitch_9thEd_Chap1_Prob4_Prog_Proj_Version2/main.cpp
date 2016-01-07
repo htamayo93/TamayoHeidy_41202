c1051e9a7afd07e02c42c8d2198166d2744a3b0c
htamayo93 <htamayo@student.rcc.edu>
2016-01-07 11:43:59 -0800

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

--- Assignments/Savitch_9thEd_Chap1_Prob4_Prog_Proj_Version2/main.cpp
+++ Assignments/Savitch_9thEd_Chap1_Prob4_Prog_Proj_Version2/main.cpp
@@ -11,6 +11,7 @@
 #include <cmath>  //Math functions
 #include <ctime>  //Time to set the random seed
 #include <iomanip>//Format Library
+#include <fstream>//Reading/Writing to a file
 using namespace std;
 
 //User Libraries
@@ -27,20 +28,42 @@ int main(int argc, char** argv) {
     srand(static_cast<unsigned int>(time(0)));
     
     //Declare Variables
+    ofstream out;//Declare a file stream object called out
     float drpTime,distance;//Time in sec and Distance in feet
     
     //Inputting the time with a random number
     drpTime=rand()%11+10;//[10,20]secs
         //insure you know what 0 mode is
+    const int SIZE=21;
+    char fileNam[SIZE]="DropDistance.dat";
+    out.open(fileNam);
+    
+    //Calculate the number
+    distance=0.5f*GRAVITY*drpTime*drpTime;
+    
+    //Output the results
+    cout<<"Drop Time = " <<drpTime<<"(sec)"<<endl;
+    cout<<fixed<<setprecision(3)<<showpoint;//Format the distance to three decimal places
+    cout<<"Free Fall Distance x2 = "<<setw(9)<<distance<<"(ft)"<<endl;
     
     //Calculate the number
     distance=0.5f*GRAVITY*pow(drpTime,2);
     
     //Output the results
-    cout<<"Drop Time = " <<drpTime<<"(sec)"<<endl;
-    cout<<fixed<<setprecision(3)<<showpoint;//Format the distance to three decimal places
-    cout<<"Free Fall Distance = "<<setw(9)<<distance<<"(ft)"<<endl;
+    cout<<"Free Fall Distance pow="<<setw(10)<<distance<<"(ft)"<<endl;
     
+    //Calculate the number of
+    distance=0.5f*GRAVITY*exp(2*log(drpTime));
+    
+    //Output the results
+    cout<<"Free Fall Distance exp(log)="<<setw(10)<<distance<<"(ft)" <<endl;
+    
+    //Output the results to a file
+    out<<fixed<<setprecision(3)<<showpoint;//Format the Distance to 3 decimals
+    out<<"Free Fall Distance exp(log)="<<setw(10)<<distance<<"(ft)"<<endl;
+    
+    //Exit stage right make sure to close files
+    out.close();
     return 0;
 }
 
