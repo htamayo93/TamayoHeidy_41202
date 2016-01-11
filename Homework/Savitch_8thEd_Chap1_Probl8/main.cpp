c7568798166b58fd57aad9046fbb53796ec76017
htamayo93 <htamayo@student.rcc.edu>
2016-01-11 00:51:15 -0800

    Homework Problems

--- Homework/Savitch_8thEd_Chap1_Probl8/main.cpp
+++ Homework/Savitch_8thEd_Chap1_Probl8/main.cpp
@@ -1,25 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
  * Author: heidy
- *
  * Created on January 10, 2016, 11:08 PM
  */
 
-#include <cstdlib>
-
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+const char QUART=25;
+const char DIME=10;
+const char NICKEL=5;
 
+int main(int argc, char** argv) {
+    int quarts, dimes, nickls, centsTo;
+    cout<<"Enter the amount of quarters."<<endl;
+    cin>>quarts;
+    cout<<"Enter the amount of dimes."<<endl;
+    cin>>dimes;
+    cout<<"Enter the amount of nickels."<<endl;
+    cin>>nickls;
+    centsTo=quarts*QUART+dimes*DIME+nickls*NICKEL;
+    cout<<"The coins are worth "<<centsTo<<" cents."<<endl;
+    
     return 0;
 }
 
