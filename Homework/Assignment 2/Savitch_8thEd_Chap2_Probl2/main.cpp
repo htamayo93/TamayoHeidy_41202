a65fed7fd030a94e4028bd4b737af454cb382175
htamayo93 <htamayo@student.rcc.edu>
2016-01-12 11:07:26 -0800

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

---
+++ Homework/Assignment 2/Savitch_8thEd_Chap2_Probl2/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Heidy Tamayo 
+ * Created on January 12, 2016, 9:08 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
