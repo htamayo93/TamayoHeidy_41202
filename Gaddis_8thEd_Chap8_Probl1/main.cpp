b3c3ff3b13b4d375b2e8a5a19fd082d2d5c3a488
htamayo93 <htamayo@student.rcc.edu>
2016-02-08 12:00:27 -0800

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

---
+++ Gaddis_8thEd_Chap8_Probl1/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on February 8, 2016, 11:45 AM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    int accnum[18]={
+        5658845,4520125,7895122,8777541,8451277,1302850,
+        8080152,4562555,5552012,5050552,7825877,1250255,
+        1005231,6545231,3852085,7576651,7881200,4581002};
+    
+    return 0;
+}
+
